6781e86e08b33da0479f35468651c5f33e87ff53
unknown <daffaatalla279@gmail.com>
2023-10-26 17:33:37 +0700

    add update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nav_item = document.getElementById("nav-item.msg");
+var form = document.getElementById("message-us");
+
+var scrollDown = nav_item.style.scrollBehavior;
+
+nav_item.onclick = function()
+{
+    nav_item.style.scrollBehavior;
+    scrollDown = form;
+}
